61c8247fe6e3b38e9adb63f38d8b54cba6952650
Jessica Xiang <jessica_x_1028@hotmail.com>
2018-09-02 09:00:35 -0400

    Init Commit

---
+++ app/src/main/java/com/example/jessi/myapplication_cs8803mas/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.jessi.myapplication_cs8803mas;
+//package com.firebasedatabase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result= num1+ num2;
+                resultTextView.setText(result+"");
+                //addNumber();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Hello, World!");
+            }
+        });
+    }
+/*
+    public void addNumber(){
+
+    }*/
+}
