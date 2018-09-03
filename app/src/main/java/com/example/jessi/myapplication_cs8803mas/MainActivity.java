234316e812e845bd39c65a3159bbd9f4cabfa18a
Jessica Xiang <jessica_x_1028@hotmail.com>
2018-09-03 19:44:08 -0400

    3rd Commit

--- app/src/main/java/com/example/jessi/myapplication_cs8803mas/MainActivity.java
+++ app/src/main/java/com/example/jessi/myapplication_cs8803mas/MainActivity.java
@@ -1,23 +1,58 @@
 package com.example.jessi.myapplication_cs8803mas;
 //package com.firebasedatabase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmailField = (EditText) findViewById(R.id.editText);
+        mPasswordField = (EditText) findViewById(R.id.editText2);
+        mLoginBtn = (Button) findViewById(R.id.button);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() !=null){
+                        startActivity(new Intent(MainActivity.this,AccountActivity.class));
+                }
+            }
+        };
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignIn();
+            }
+        });
         Button addBtn = (Button) findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,12 +68,53 @@ public class MainActivity extends AppCompatActivity {
                 //addNumber();
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("message");
-                myRef.setValue("Hello, World! Bao Bao");
+                myRef.setValue("You clicked + !");
+            }
+        });
+
+        Button minusBtn = (Button) findViewById(R.id.minusBtn);
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result= num1 - num2;
+                resultTextView.setText(result+"");
+                //addNumber();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("You clicked - !");
             }
         });
     }
-/*
-    public void addNumber(){
 
-    }*/
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void startSignIn(){
+        String email=mEmailField.getText().toString();
+        String password= mPasswordField.getText().toString();
+
+        if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password)) {
+                Toast.makeText(MainActivity.this,"Fields are empty", Toast.LENGTH_LONG).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this,"Sign in Problem", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+    }
 }
